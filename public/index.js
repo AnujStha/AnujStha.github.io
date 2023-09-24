f8d41bae23b2ac59dfe5a1e57f6b7f7f1dff2110
AnujStha <anujshrestha.lubhoo@gmail.com>
2023-09-24 20:07:26 +0545

    run shader by text

--- public/index.js
+++ public/index.js
@@ -17,9 +17,33 @@ function getPoints(count){
 }
 
 async function loadShaderAndRun(){
-    const vertShader=await fetch("./Shaders/vert.vert");
-    const fragShader=await fetch("./Shaders/frag.frag");
-    run(await vertShader.text(),await fragShader.text())
+    // const vertShader=await fetch("./Shaders/vert.vert");
+    // const fragShader=await fetch("./Shaders/frag.frag");
+    // run(await vertShader.text(),await fragShader.text())
+
+    run(
+        `
+        precision mediump float;
+
+        attribute vec3 position;
+        varying vec3 vColor;
+
+        void main() {
+            gl_Position = vec4(position, 1);
+            gl_PointSize=1.0;
+            vColor=vec3(position.xy,1);
+        }
+        `,
+        `
+        precision mediump float;
+
+        varying vec3 vColor;
+
+        void main() {
+            gl_FragColor = vec4(vColor, 1.0);
+        }
+        `
+    )
 }
 
 function run(vert,frag){
